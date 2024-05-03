028473bb6dc721c7b8b1e60b2f81c01a43a0e41a
jameshu <james.hu@barecovetech.com>
2024-05-03 18:00:24 +0800

    feat: base

---
+++ .vitepress/config.ts
@@ -0,0 +1,7 @@
+export default {
+    // 应用级配置选项
+    lang: 'en-US',
+    title: 'Git Clone Pack',
+    description: 'Download and extract a git repository (GitHub, GitLab, Bitbucket) from node.',
+    base: '/git-clone-pack/',
+}
